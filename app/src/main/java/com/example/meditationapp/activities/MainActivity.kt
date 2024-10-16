917f5ae497d85793c644508d4b0901977149fb00
Terry Tess <terrywamboi.ke@gmail.com>
2024-10-16 09:57:12 +0300

    Welcome fragment

--- app/src/main/java/com/example/meditationapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/meditationapp/activities/MainActivity.kt
@@ -2,10 +2,7 @@ package com.example.meditationapp.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.meditationapp.R
 import com.example.meditationapp.databinding.ActivityMainBinding
@@ -21,32 +18,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
-        binding.startButton.setOnClickListener {
-            val navController = findNavController(R.id.nav_host_fragment)
-            navController.navigate(R.id.SignInUpFragment)
-
-
-        }
-
-        //getting fragment to load
+        // Get the fragment to load from the intent
         val fragmentToLoad = intent.getStringExtra(FRAGMENT_TO_BE_SHOWN)
-        fragmentToLoad?.let { fragment->
+        fragmentToLoad?.let { fragment ->
             navigateToFragment(fragment)
         }
     }
 
+    // Method to handle navigation based on the fragment name
     private fun navigateToFragment(fragment: String) {
-
-        when(fragment){
-            "welcomeFragment"->navController.navigate(R.id.WelcomeFragment)
+        when (fragment) {
+            "welcomeFragment" -> navController.navigate(R.id.welcomeFragment)
+            else -> {
+                // Handle any other fragments (if added in the future)
+            }
         }
-
     }
 }
-
